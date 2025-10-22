ef6a620e157741199a064f87ef4e4b38e7f673b4
Diego Villalba <diego.villalba01@uptc.edu.co>
2025-10-22 14:15:46 -0500

    lista, detalles y agrega a favoritos

--- src/utils/pokemonUtils.js
+++ src/utils/pokemonUtils.js
@@ -5,7 +5,7 @@ export const processPokemonData = (pokemon) => {
     name: pokemon.name,
     types: pokemon.pokemon_v2_pokemontypes?.map(type => type.pokemon_v2_type.name) || [],
     image: pokemon.pokemon_v2_pokemonsprites?.[0]?.sprites ? 
-      JSON.parse(pokemon.pokemon_v2_pokemonsprites[0].sprites).front_default : 
+      pokemon.pokemon_v2_pokemonsprites[0].sprites.front_default : 
       null,
     generation: pokemon.pokemon_v2_pokemonspecy?.pokemon_v2_generation?.name || 'Unknown',
     height: pokemon.height,
